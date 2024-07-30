4fe19b51204f0ba94320a5d36ba982e4731335b8
Margaret Alade <alade.margaret@yandex.ru>
2024-07-30 06:39:17 +0300

    Initial Commit

---
+++ Task-2.1-Decorator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-2.1-Decorator
+//
+//  Created by Маргарет  on 30.07.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
